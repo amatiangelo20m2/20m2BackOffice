af71e324d612cf035646eff0427a138f1b120435
amatiangelo20m2 <amati.angelo@20m2official.it>
2024-09-23 19:42:36 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  defaultauth: 'fackbackend',
+  firebaseConfig: {
+    apiKey: 'AIzaSyAIrRUP8z39yFhjsfnLsxwDhzxzAguzHvI',
+    authDomain: 'vixon-angular.firebaseapp.com',
+    databaseURL: '',
+    projectId: 'vixon-angular',
+    storageBucket: 'vixon-angular.appspot.com',
+    messagingSenderId: '822277458131',
+    appId: '1:822277458131:web:5ec3bee2543b96567aac26',
+    measurementId: 'G-YPD27EXC6H'
+  }
+};
